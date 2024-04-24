1ba3f6e4f10e7b857b9e15be7d5a731c5727f097
Felipe Lima <felipe.lima@gmail.com>
2024-04-24 13:57:46 -0700

    mlp

--- src/main/kotlin/nn.kt
+++ src/main/kotlin/nn.kt
@@ -1,7 +1,7 @@
 import kotlin.random.Random
 
 // nin - number of inputs
-fun Neuron(nin: Int): (List<Double>) -> Value {
+fun Neuron(nin: Int): (List<Value>) -> Value {
   // weights
   val w = (0..<nin).map { Value(Random.nextDouble(-1.0, 1.0)) }
   // bias
@@ -18,9 +18,22 @@ fun Neuron(nin: Int): (List<Double>) -> Value {
 
 // nin - number of inputs
 // nout - number of outputs
-fun Layer(nin: Int, nout: Int): (List<Double>) -> List<Value> {
-  val neurons = (0..<nout).map { Neuron(nin) }
+fun Layer(nin: Int, nouts: Int): (List<Value>) -> List<Value> {
+  val neurons = (0..<nouts).map { Neuron(nin) }
   return { x ->
     neurons.map { n -> n(x) }
+  }
+}
+
+// multi-layer perceptron
+fun MLP(nin: Int, nouts: List<Int>): (List<Double>) -> List<Value> {
+  val sz = listOf(nin) + nouts
+  val layers = nouts.indices.map { Layer(sz[it], sz[it+1]) }
+  return { x ->
+    var ret = x.map { Value(it) }
+    for (layer in layers) {
+      ret = layer(ret)
+    }
+    ret
   }
 }
--- src/test/kotlin/nnTest.kt
+++ src/test/kotlin/nnTest.kt
@@ -5,7 +5,7 @@ class nnTest {
   @Test
   fun `test neuron`() {
     val n = Neuron(2)
-    val act = n(listOf(2.0, 3.0))
+    val act = n(listOf(Value(2.0), Value(3.0)))
     val data = act.data
     assertThat(data).isNotEqualTo(0.0)
   }
@@ -13,7 +13,14 @@ class nnTest {
   @Test
   fun `test layer`() {
     val layer = Layer(2, 3)
-    val out = layer(listOf(2.0, 3.0))
+    val out = layer(listOf(Value(2.0), Value(3.0)))
+    println(out.map { it.data })
+  }
+
+  @Test
+  fun `test mlp`() {
+    val mlp = MLP(3, listOf(4, 4, 1))
+    val out = mlp(listOf(2.0, 3.0, -1.0))
     println(out.map { it.data })
   }
 }
